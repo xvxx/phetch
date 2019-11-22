f72ba1f4f66e15a94d044e5e888f2f75a2907dd6
dvkt <c@dvkt.io>
2019-11-22 00:18:07 -0800

    render()

--- src/main.rs
+++ src/main.rs
@@ -17,33 +17,7 @@ fn main() {
             stream.write("\r\n".as_ref()).unwrap();
             let mut buf = String::new();
             stream.read_to_string(&mut buf);
-            let mut start = true;
-            let mut skip_to_end = false;
-            for c in buf.chars() {
-                if start {
-                    match c {
-                        'i' => print!("\x1B[93m"),
-                        'h' => print!("\x1B[94m"),
-                        '0' => print!("\x1B[95m"),
-                        '1' => print!("\x1B[96m"),
-                        _ => print!("\x1B[0m"),
-                    }
-                    start = false
-                } else if skip_to_end {
-                    if c == '\n' {
-                        println!("");
-                        start = true;
-                        skip_to_end = false;
-                    }
-                } else if c == '\t' {
-                    skip_to_end = true;
-                } else {
-                    print!("{}", c);
-                    if c == '\n' {
-                        start = true
-                    }
-                }
-            }
+            render(&buf);
 
             for c in stdin.keys() {
                 // Clear the current line.
@@ -81,4 +55,34 @@ fn main() {
         .map_err(|err| {
             eprintln!("err: {}", err);
         });
+}
+
+fn render(buf: &str) {
+    let mut start = true;
+    let mut skip_to_end = false;
+    for c in buf.chars() {
+        if start {
+            match c {
+                'i' => print!("\x1B[93m"),
+                'h' => print!("\x1B[94m"),
+                '0' => print!("\x1B[95m"),
+                '1' => print!("\x1B[96m"),
+                _ => print!("\x1B[0m"),
+            }
+            start = false
+        } else if skip_to_end {
+            if c == '\n' {
+                println!("\r");
+                start = true;
+                skip_to_end = false;
+            }
+        } else if c == '\t' {
+            skip_to_end = true;
+        } else {
+            print!("{}", c);
+            if c == '\n' {
+                start = true
+            }
+        }
+    }
 }
