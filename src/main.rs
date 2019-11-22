47212040c193c69d4536fea86f0742fe65ee5212
dvkt <c@dvkt.io>
2019-11-21 23:43:20 -0800

    basic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![allow(unused_must_use)]
+
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    TcpStream::connect("phkt.io:70")
+        .and_then(|mut stream| {
+            stream.write("\r\n".as_ref()).unwrap();
+            let mut buf = String::new();
+            stream.read_to_string(&mut buf);
+            println!("{}", buf);
+            Ok(())
+        })
+        .map_err(|err| {
+            eprintln!("err: {}", err);
+        });
 }
