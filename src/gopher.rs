f64cba41d5019432a2935ce39d39697c4c94ef82
dvkt <c@dvkt.io>
2019-12-21 18:47:20 -0800

    debug log

--- src/gopher.rs
+++ src/gopher.rs
@@ -99,6 +99,23 @@ pub fn char_for_type(t: Type) -> Option<char> {
     })
 }
 
+#[allow(unused_macros)]
+macro_rules! log {
+    ($e:expr) => {{
+        if let Ok(mut file) = std::fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("phetch.log")
+    {
+        file.write($e.as_ref());
+        file.write(b"\n");
+    }
+    }};
+    ($e:expr, $($y:expr),*) => {
+        log!(format!($e, $($y),*));
+    };
+}
+
 macro_rules! error {
     ($e:expr) => {
         std::io::Error::new(std::io::ErrorKind::Other, $e)
