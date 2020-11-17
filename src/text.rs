16034d184e1a0cca9a130c3e6f54412520d2aaf0
chris west <c@xvxx.io>
2020-11-16 16:01:43 -0800

    wrap constrains line width when active

--- src/text.rs
+++ src/text.rs
@@ -136,10 +136,13 @@ impl View for Text {
         let wrap = self.config.read().unwrap().wrap;
         let longest = if self.longest > MAX_COLS {
             MAX_COLS
-        } else if wrap < self.longest {
-            wrap
         } else {
             self.longest
+        };
+        let longest = if wrap > 0 && self.longest > wrap {
+            wrap
+        } else {
+            longest
         };
         let indent = if cols >= longest && cols - longest <= 6 {
             String::from("")
